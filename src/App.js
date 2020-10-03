932c8e9c9c039c09215b22efe46bb24c82f900ee
fer <ferp@protonmail.ch>
2020-10-03 12:14:51 +0200

    first exercise complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
+import "./App.css";
+import { select } from "d3";
+
+const initialData = [25, 45, 30, 74, 38];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState(initialData)
+  const svgRef = useRef();
+  useEffect(() => {
+    const svg = select(svgRef.current);
+    svg
+      .selectAll("circle")
+      .data(data)
+      .join("circle")
+      .attr("r", value => value)
+      .attr("cx", value => value * 2)
+      .attr("cy", value => value * 2)
+      .attr("stroke", "red")
+;
+  }, [data]);
+  return <React.Fragment>
+    <svg ref={svgRef}></svg>
+    <br/>
+    <button onClick={() => setData(data.map( val => val + 5))}>Update Data</button>
+    <button onClick={() => setData(data.filter( value => value  < 50))}> Filter Data</button>
+    </React.Fragment>;
 }
 
 export default App;
